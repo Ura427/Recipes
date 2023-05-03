43f8eab808b30d697d97f9b7d45494f0dc9596cf
Yura <urakost427@gmail.com>
2023-05-03 21:18:38 +0300

    Add project files.

---
+++ Recipes/Models/Dish.cs
@@ -0,0 +1,11 @@
+﻿namespace Recipes.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingridients { get; set; }
+        public string Recipe { get; set; }
+        public string Link { get; set; }
+    }
+}
